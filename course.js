9fa59f07b084dcfe97d5cdac3e0e3f4297d75764
なぎと-nagito- <hello@nagito.work>
2024-04-11 13:57:02 +0900

    thery updata

--- course.js
+++ course.js
@@ -1,6 +1,8 @@
+console.log("course.js Strted");
 chrome.storage.sync.get({"selectkey":"" }, function (items) {
   //0.5秒後にselectkeyの値を取得して、その値のidをクリックする
   setTimeout(function(){
     document.getElementById(items.selectkey).click();
   },500);
 });
+console.log("course.js Ended");
