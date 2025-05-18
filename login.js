3afb7387a00478bf271e243012180b50454c406d
なぎと-nagito- <hello@nagito.work>
2025-05-18 23:32:07 +0900

    Update login.js

--- login.js
+++ login.js
@@ -11,11 +11,11 @@ chrome.storage.sync.get(['user_id', 'user_pass'], function (value) {
 
     if (USER_ID && USER_PASS) {
         //ログインボタンをクリック
-        if (document.getElementById('loginerrormessage').innerHTML != "不正なログインです。再度ログインしてください。") {
+        if (document.getElementById('loginerrormessage').innerHTML == "あなたのセッションがタイムアウトしました。再度ログインしてください。") {
+            document.getElementById('loginbtn').click();
+        }else if        (document.getElementById('loginerrormessage').innerHTML != "不正なログインです。再度ログインしてください。") {
             document.getElementById('loginbtn').click();
         } else if (!document.getElementById('loginerrormessage')) {
-            document.getElementById('loginbtn').click();
-        } else if (document.getElementById('loginerrormessage').innerHTML == "あなたのセッションがタイムアウトしました。再度ログインしてください。") {
             document.getElementById('loginbtn').click();
         }
     }
